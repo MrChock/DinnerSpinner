f7820ab4c32e83e8be037df928e9eaf305110f6f
MrChock <student10@js-c02kp2dlf5n7.punahou.edu>
2016-06-30 11:05:28 -1000

    IT WORKS!
    WE linked all of the days in html and java script, and not it randomizes

--- js/index.js
+++ js/index.js
@@ -1,13 +1,34 @@
-	var Main_Dish = ["Meat Loaf", "Steak", "Lau Lau", "Shou Chicken", 
-"Grilled Salmon", "Shrimp Scampy Pasta", "Spinach", "Garlic", "Kalua Pig", "Olive"];
+	var Main_Dish = ["Meat Loaf", "Steak", "Lau Lau", "Shoyu Chicken", 
+"Grilled Salmon", "Shrimp Scampi" , "Kalua Pig"];
 
-var Side_Dish = ["Rice", "Mash Potato", "Salad", "Grilled Vegies", 
-"Poi", "Lomi Lomi Salmon", "Creamed Spinach", "Mac Salad", "Kalua Pig", "Olive"];
+var Side_Dish = ["Rice", "Mash Potato", "Salad", "Grilled Veggies", 
+"Poi", "Lomi Salmon", "Creamed Spinach", "Mac Salad"];
 
 
 function pickTopping(){
 	var Main_index = Math.floor(Math.random()*Main_Dish.length);
 	var Side_index = Math.floor(Math.random()*Side_Dish.length);
-	document.getElementById('MondayMain').innerHTML = "<h1>"+main_dish[Main_Dish]+" and "+
+	document.getElementById('MondayMain').innerHTML = Main_Dish[Main_index];
+	document.getElementById('MondaySide').innerHTML = Side_Dish[Side_index];
+
+	var Main_index = Math.floor(Math.random()*Main_Dish.length);
+	var Side_index = Math.floor(Math.random()*Side_Dish.length);
+	document.getElementById('TuesdayMain').innerHTML = Main_Dish[Main_index];
+	document.getElementById('TuesdaySide').innerHTML = Side_Dish[Side_index];
+
+	var Main_index = Math.floor(Math.random()*Main_Dish.length);
+	var Side_index = Math.floor(Math.random()*Side_Dish.length);
+	document.getElementById('WednesdayMain').innerHTML = Main_Dish[Main_index];
+	document.getElementById('WednesdaySide').innerHTML = Side_Dish[Side_index];
+
+	var Main_index = Math.floor(Math.random()*Main_Dish.length);
+	var Side_index = Math.floor(Math.random()*Side_Dish.length);
+	document.getElementById('ThursdayMain').innerHTML = Main_Dish[Main_index];
+	document.getElementById('ThursdaySide').innerHTML = Side_Dish[Side_index];
+
+	var Main_index = Math.floor(Math.random()*Main_Dish.length);
+	var Side_index = Math.floor(Math.random()*Side_Dish.length);
+	document.getElementById('FridayMain').innerHTML = Main_Dish[Main_index];
+	document.getElementById('FridaySide').innerHTML = Side_Dish[Side_index];
 }
 
